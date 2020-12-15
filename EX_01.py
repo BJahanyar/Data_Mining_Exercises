c6c5c73a73fa7189e45612edea323886d98cfabc
Bahar Jahanyar <49306136+BJahanyar@users.noreply.github.com>
2020-12-15 23:08:22 +0330

    Add files via upload

---
+++ EX_01.py
@@ -0,0 +1,13 @@
+print ("Please Enter a String")
+x = input() # یک رشته بگیر
+thisdict = {} # یک آرایه دو بعدی تعریف کن برای نگه داری کارکتر ها و تعدادشان
+for ch in x: # هر کاراکتر در رشته را پشمایش کن
+    if ch not in thisdict.keys() : #اگر کاراکتر در کلیدهای دیکشنری وجود  نداشت
+        thisdict.update({ ch : "1" }) #مقدار را 1 بگذار
+        pass
+    else: # اگر کاراکتر در کلید دیکشنری وجود داشت
+        #مقدار آرایه را بگیر بعلاوه 1 کن و آن را مجددا در متغییر کاراکتر بریز و آن را آپدیت کن
+        thisdict.update({ch : int(thisdict.get(ch)) +1  })
+        pass
+    pass
+print(thisdict)
