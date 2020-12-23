d2c1ad5732768bfdf25cdbec5e5108226e40befb
Bahar Jahanyar <49306136+BJahanyar@users.noreply.github.com>
2020-12-23 08:09:55 +0330

    Update EX_05.py

--- EX_05.py
+++ EX_05.py
@@ -5,9 +5,7 @@ for i in range(Row): #حلقه را تا کمتر از عدد سطر تکرار 
     TempRow = [] # یک آرایه برای نگه داری سطر ایجاد کن 
     for j in range(Col): # حلقه را تا کمتر از عدد ستون تکرار کن
         TempRow.append(int(input())) #عددی که کاربر وارد کرده را به آرایه تمپ اضافه کن
-        pass # حلقه تمام شده است
     Main.append(TempRow)  #عددی که کاربر وارد کرده را به آرایه مین اضافه کن
-    pass
 print('___________Main Matris___________' )
 print(Main) #آرایه مین را چاپ کن
 
@@ -17,44 +15,5 @@ for i in range(Row):
         if Main[i][j] != 0:
             Temp = [i,j,Main[i][j]]
             Mysparse.append (Temp)
-            pass
-        pass
-    pass
 print('___________Sparse Matris___________' )
-print(Mysparse) #آرایه مین را چاپ کن
-
-MyTranspose = []
-for i in range(Col):
-    TempTrans = []
-    for j in range(Row):
-        TempTrans.append(Main[j][i])        
-        pass
-    MyTranspose.append (TempTrans)
-    pass
-
-print('___________MyTranspose Matris___________' )
-print(MyTranspose) #آرایه مین را چاپ کن
-
-MySum = []
-for i in range(Row):
-    Tempsum = []
-    for j in range(Col):
-        Tempsum.append(Main[i][j] + Main[i][j])
-        pass
-    MySum.append (Tempsum)
-    pass
-
-print('___________MySum Matris___________' )
-print(MySum) #آرایه مین را چاپ کن
-
-MyMulti = []
-for i in range(Row):
-    TempMult = []
-    for j in range(Col):
-        TempMult.append(Main[i][j] * Main[i][j])
-        pass
-    MyMulti.append (TempMult)
-    pass
-
-print('___________MyMulti Matris___________' )
-print(MyMulti) #آرایه مین را چاپ کن
+print(Mysparse)
